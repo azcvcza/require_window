c52e34274b89e24465527a200eb9e7688244b3b3
azcvcza <283940802@qq.com>
2018-10-18 02:33:50 +0800

    doc: complete first window

--- js/window.js
+++ js/window.js
@@ -1,19 +1,35 @@
 define(['jquery'],function($){
-    function Window(){}
+    function Window(){
+        this.cfg = {
+            width:500,
+            height:300,
+            title:"system call",
+            content:"",
+            handler:null,
+        }
+    }
 
     Window.prototype={
-        alert:function(content,handler){
-            var boundingBox =$('<div></div>');
+        alert:function(cfg){
+            var config = $.extend(this.cfg,cfg);
+            console.log(config)
+            var boundingBox =$('<div class="window_boundingBox">'
+            +'<div class="window_header">'+config.title+'</div>'
+            +'<div class="window_body">'+config.content+'</div>'
+            +'<div class="window_footer"><input type="button" value="确定"/></div>'
+            +'</div>');
             //直接搞 <div class="xxx"></div>就失败了
-            boundingBox.addClass("window_boundingBox")
-            boundingBox.appendTo('body');
-            boundingBox.html(content);
-            
-            var button = $('<input type="button" value="确定"></input>')
-            button.appendTo(boundingBox);
+            var button = boundingBox.find(".window_footer input")
             button.click(function(){
-                handler&&handler();
+                config.handler && config.handler();
                 boundingBox.remove();
+            })
+            boundingBox.appendTo('body');
+            boundingBox.css({
+                width:config.width+"px",
+                height:config.height+"px",
+                left:(config.x || (window.innerWidth - config.width)/2)+"%",
+                top:(config.y || (window.innerHeight - config.height)/2)+"%",
             })
             
         },
--- js/window_main.js
+++ js/window_main.js
@@ -1,12 +1,22 @@
 require.config({
-    paths:{
-        jquery:'jquery-3.1.0'
+    paths: {
+        jquery: 'jquery-3.1.0'
     }
 });
-require(['jquery','window'],function($,w)
-{
-    new w.Window().alert("welcome",function(){
-        alert("click the button")
-    });
-    
+require(['jquery', 'window'], function ($, w) {
+    $('#click').click(function () {
+        new w.Window().alert({
+            width: 500,
+            height: 250,
+            x:50,
+            y:30,
+            title:"系统消息",
+            content: "alert box",
+            handler: function(){
+                alert("window closed")
+            },
+        });
+    })
+
+
 })
