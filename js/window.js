7577a720dcec5a24d07f7398eedeea786792881e
azcvcza <283940802@qq.com>
2018-10-18 14:22:30 +0800

    feature: add a close btn to the window

--- js/window.js
+++ js/window.js
@@ -1,42 +1,71 @@
-define(['jquery'],function($){
-    function Window(){
+define(['jquery'], function ($) {
+    function Window() {
         this.cfg = {
-            width:500,
-            height:300,
-            title:"system call",
-            content:"",
-            handler:null,
+            width: 500,
+            height: 300,
+            title: "system call",
+            content: "",
+            handler: null,
+            hasCloseBtn: false,
+            hasMask: true,
+            test4AlertBtn: "确定",
+            handler4AlertBtn: null,
+            handlerCloseBtn: null,
         }
     }
 
-    Window.prototype={
-        alert:function(cfg){
-            var config = $.extend(this.cfg,cfg);
-            console.log(config)
-            var boundingBox =$('<div class="window_boundingBox">'
-            +'<div class="window_header">'+config.title+'</div>'
-            +'<div class="window_body">'+config.content+'</div>'
-            +'<div class="window_footer"><input type="button" value="确定"/></div>'
-            +'</div>');
-            //直接搞 <div class="xxx"></div>就失败了
-            var button = boundingBox.find(".window_footer input")
-            button.click(function(){
+    Window.prototype = {
+        alert: function (cfg) {
+            var config = $.extend(this.cfg, cfg);
+
+            var boundingBox = $('<div class="window_boundingBox">' +
+                '<div class="window_header">' + config.title + '</div>' +
+                '<div class="window_body">' + config.content + '</div>' +
+                '<div class="window_footer"><input type="button" value="确定"/></div>' +
+                '</div>');
+
+            var button = boundingBox.find(".window_footer input");
+            var mask = null;
+
+
+            button.click(function () {
                 config.handler && config.handler();
                 boundingBox.remove();
+                mask & mask.remove();
             })
+
             boundingBox.appendTo('body');
+            $.extend(this.config,config);
             boundingBox.css({
-                width:config.width+"px",
-                height:config.height+"px",
-                left:(config.x || (window.innerWidth - config.width)/2)+"%",
-                top:(config.y || (window.innerHeight - config.height)/2)+"%",
-            })
-            
+                width: config.width + "px",
+                height: config.height + "px",
+                left: (config.x || (window.innerWidth - config.width) / 2) + "%",
+                top: (config.y || (window.innerHeight - config.height) / 2) + "%",
+            });
+            if (config.hasMask) {
+                mask = $('<div class="window_mask"></div>');
+                mask.appendTo('body');
+            }
+            if (config.hasCloseBtn) {
+                
+                var closeBtn = $('<span class="window_closeBtn">关闭</span>');
+                closeBtn.appendTo($('.window_boundingBox'));
+                closeBtn.click(function () {
+                    config.handler4CloseBtn && config.handler4CloseBtn(); //弹出窗口的关闭按钮处理函数
+                    $('.window_boundingBox').remove();
+                    mask && mask.remove();
+                })
+            }
+            if (config.skinClassName) {
+                boundingBox.addClass(config.skinClassName);
+            }
+
+
         },
-        confirm:function(){},
-        prompt:function(){},
+        confirm: function () {},
+        prompt: function () {},
     }
-    return{
-        Window:Window
+    return {
+        Window: Window
     }
 })
--- js/window_main.js
+++ js/window_main.js
@@ -6,15 +6,24 @@ require.config({
 require(['jquery', 'window'], function ($, w) {
     $('#click').click(function () {
         new w.Window().alert({
-            width: 500,
-            height: 250,
-            x:50,
-            y:30,
-            title:"系统消息",
-            content: "alert box",
-            handler: function(){
-                alert("window closed")
-            },
+            title:'提示',
+			content:'Welcome!!!',
+			handler:function(){
+				alert('you click the button');
+			},
+			width:300,
+			height:150,
+			y:30,
+			x:55,
+			hasCloseBtn:true,
+			text4AlertBtn:'OK',
+			skinClassName:'window_skin_a',
+			handler4AlertBtn:function(){
+				alert('你点击了确定按钮');
+			},
+			handler4CloseBtn:function(){
+				alert('你点击了关闭按钮');
+			}
         });
     })
 
